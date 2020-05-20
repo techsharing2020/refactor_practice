07f17952ce950acf5e262c8840e89518b932438a
tim <tim.huang61@gmail.com>
2020-05-21 01:06:48 +0800

    替換CalculateTotalAmount & CalculateAmount傳入的參數

--- Drama/Statement.cs
+++ Drama/Statement.cs
@@ -29,13 +29,13 @@ namespace Drama
         {
             var result = $"Statement for {data.Customer}\n";
 
-            var volumeCredits = CalCulateVolumeCredits(invoice, playDic);
-            var totalAmount   = CalculateTotalAmount(invoice, playDic);
+            var volumeCredits = CalculateVolumeCredits(invoice, playDic);
+            var totalAmount   = CalculateTotalAmount(data.PerformanceDetails);
 
-            foreach (var perf in data.PerformanceDetails)
+            foreach (var detail in data.PerformanceDetails)
             {
                 result +=
-                    $" {perf.Play.Name}: {(CalculateAmount(perf.Play, perf) / 100).ToString("C2", new CultureInfo("en-US"))} ({perf.Audience} seats)\n";
+                    $" {detail.Play.Name}: {(CalculateAmount(detail) / 100).ToString("C2", new CultureInfo("en-US"))} ({detail.Audience} seats)\n";
             }
 
             result += $"Amount owed is {(totalAmount / 100).ToString("C2", new CultureInfo("en-US"))}\n";
@@ -44,19 +44,12 @@ namespace Drama
             return result;
         }
 
-        private static decimal CalculateTotalAmount(Invoice invoice, Dictionary<string, Play> playDic)
+        private static decimal CalculateTotalAmount(IEnumerable<PerformanceDetail> details)
         {
-            var totalAmount = 0m;
-            foreach (var perf in invoice.Performances)
-            {
-                var play = playDic[perf.PlayID];
-                totalAmount += CalculateAmount(play, perf);
-            }
-
-            return totalAmount;
+            return details.Sum(CalculateAmount);
         }
 
-        private static decimal CalCulateVolumeCredits(Invoice invoice, Dictionary<string, Play> playDic)
+        private static decimal CalculateVolumeCredits(Invoice invoice, Dictionary<string, Play> playDic)
         {
             var volumeCredits = 0m;
             foreach (var perf in invoice.Performances)
@@ -79,30 +72,30 @@ namespace Drama
             return credits;
         }
 
-        private static decimal CalculateAmount(Play play, Performance perf)
+        private static decimal CalculateAmount(PerformanceDetail detail)
         {
             decimal result;
-            switch (play.Type)
+            switch (detail.Play.Type)
             {
                 case PlayType.Tragedy:
                     result = 40000;
-                    if (perf.Audience > 30)
+                    if (detail.Audience > 30)
                     {
-                        result += 1000 * (perf.Audience - 30);
+                        result += 1000 * (detail.Audience - 30);
                     }
 
                     break;
                 case PlayType.Comedy:
                     result = 30000;
-                    if (perf.Audience > 20)
+                    if (detail.Audience > 20)
                     {
-                        result += 10000 + 500 * (perf.Audience - 20);
+                        result += 10000 + 500 * (detail.Audience - 20);
                     }
 
-                    result += 300 * perf.Audience;
+                    result += 300 * detail.Audience;
                     break;
                 default:
-                    throw new Exception($"unknown type: {play.Type}");
+                    throw new Exception($"unknown type: {detail.Play.Type}");
             }
 
             return result;
