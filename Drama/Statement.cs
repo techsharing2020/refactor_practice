1186b245a1c016c10cf99c2b9493cc97b02f9ef5
tim <tim.huang61@gmail.com>
2020-05-21 00:25:54 +0800

    extract CalculateAmount

--- Drama/Statement.cs
+++ Drama/Statement.cs
@@ -16,27 +16,7 @@ namespace Drama
             foreach (var perf in invoice.Performances)
             {
                 var play = playDic[perf.PlayID];
-                decimal thisAmount;
-                switch (play.Type)
-                {
-                    case PlayType.Tragedy:
-                        thisAmount = 40000;
-                        if (perf.Audience > 30)
-                        {
-                            thisAmount += 1000 * (perf.Audience - 30);
-                        }
-                        break;
-                    case PlayType.Comedy:
-                        thisAmount = 30000;
-                        if (perf.Audience > 20)
-                        {
-                            thisAmount += 10000 + 500 * (perf.Audience - 20);
-                        }
-                        thisAmount += 300 * perf.Audience;
-                        break;
-                    default:
-                        throw new Exception($"unknown type: { play.Type }");
-                }
+                var thisAmount = CalculateAmount(play, perf);
 
                 volumeCredits += Math.Max(perf.Audience - 30, 0);
                 if (play.Type == PlayType.Comedy)
@@ -50,6 +30,35 @@ namespace Drama
 
             result += $"Amount owed is { (totalAmount / 100).ToString("C2", new CultureInfo("en-US"))}\n";
             result += $"You earned { volumeCredits} credits!\n";
+
+            return result;
+        }
+
+        private static decimal CalculateAmount(Play play, Performance perf)
+        {
+            decimal result;
+            switch (play.Type)
+            {
+                case PlayType.Tragedy:
+                    result = 40000;
+                    if (perf.Audience > 30)
+                    {
+                        result += 1000 * (perf.Audience - 30);
+                    }
+
+                    break;
+                case PlayType.Comedy:
+                    result = 30000;
+                    if (perf.Audience > 20)
+                    {
+                        result += 10000 + 500 * (perf.Audience - 20);
+                    }
+
+                    result += 300 * perf.Audience;
+                    break;
+                default:
+                    throw new Exception($"unknown type: {play.Type}");
+            }
 
             return result;
         }
